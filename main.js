cc63c76ba15b8990345f5c53a0d2d2a9b07735f5
Declan Vea <declanpvea@gmail.com>
2017-08-08 21:50:28 -0400

    fixed styling and added funcationality to buttons

--- main.js
+++ main.js
@@ -1,18 +1,34 @@
-function num(fn) {
-  return function (x, y) {
-    return fn(Number(x), Number(y));
-  }
+
+let single = document.getElementsByClassName("single");
+let operations = document.getElementsByClassName("operations");
+let clear = document.getElementById("clear");
+let display = document.querySelector("input");
+
+for (let i = 0; i < single.length; i++) {
+  single[i].addEventListener("click", function() {
+      let inputValue = entryText.value;
+      let buttonValue = this.textContent;
+        entryText.value += buttonValue;
+        // console.log(entryText.value);
+  })
 }
 
-let add = num(function(x, y) {
-  return x + y;
-});
-let sub = num(function(x, y) {
-  return x - y;
-});
-let mult = num(function(x, y) {
-  return x * y;
-});
-let divide = num(function(x, y) {
-  return x / y;
-});
+for (let i = 0; i < operations.length; i++) {
+   operations[i].addEventListener("click", function() {
+     let inputValue = entryText.value;
+     let buttonValue = this.textContent;
+     if (buttonValue !== "=") {
+       entryText.value += buttonValue;
+     }
+  })
+}
+
+function total(){
+  entryText.value = eval(entryText.value) || "";
+}
+
+function clearDisplay(){
+  let display = document.querySelector("input");
+  let buttonValue = this.textContent;
+    entryText.value = " ";
+}
