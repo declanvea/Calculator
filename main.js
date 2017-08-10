60d7c5b6abdb4789d6153cc094dfce60e5024768
Declan Vea <declanpvea@gmail.com>
2017-08-10 11:03:57 -0400

    added comments

--- main.js
+++ main.js
@@ -3,16 +3,16 @@ let single = document.getElementsByClassName("single");
 let operations = document.getElementsByClassName("operations");
 let clear = document.getElementById("clear");
 
-
+// Sets the value of the button pushed as its unique value and adds it to the total value string
 for (let i = 0; i < single.length; i++) {
   single[i].addEventListener("click", function() {
       let inputValue = entryText.value;
       let buttonValue = this.textContent;
         entryText.value += buttonValue;
-        // console.log(entryText.value);
   })
 }
 
+// Set the value of the operator to its unique value and adds it to the total value string
 for (let i = 0; i < operations.length; i++) {
    operations[i].addEventListener("click", function() {
      let inputValue = entryText.value;
@@ -23,12 +23,13 @@ for (let i = 0; i < operations.length; i++) {
   })
 }
 
+// Using eval will trigger the equal function to take place, this handles order of operations too
 function total(){
   entryText.value = eval(entryText.value) || "";
 }
 
-function clearDisplay(){
-  let buttonValue = this.textContent;
+// Set the entry value to " " will clear the screen
+function clearEntry(){
     entryText.value = " ";
 }
 
